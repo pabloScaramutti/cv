9ed3fb1d5a221a9f673add463c0ec7039e931b29
Pablo Scaramutti <pabloscaramutti@gmail.com>
2022-02-22 15:46:42 -0300

    Change baseURL for deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+const base = "/cv/"
+
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()], base
 })
